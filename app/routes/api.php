a0f45f0ff4d3ff321bf91c2162de1c42f4081951
Furkan Kulaber <furkan@motaword.com>
2022-10-02 15:57:01 +0300

    add docker

--- app/routes/api.php
+++ app/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/heartbeat', function (){
+    return \Illuminate\Support\Facades\Response::json(
+        [
+            "status" => true,
+            "version" => app()->version()
+        ]
+    );
+});
